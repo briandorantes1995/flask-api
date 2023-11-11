a84395dd99dc85c021042eebac61bf63544cc1fb
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-11-11 13:22:05 -0600

    nueva api 11

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 main.py
 - creates an application instance and runs the dev server
 """
+from waitress import serve
 
 if __name__ == '__main__':
     from application import create_app
 
     app = create_app()
-    app.run()
+    serve(app, port=8080)
