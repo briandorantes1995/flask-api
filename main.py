6ef61e5e47ea07cf7bfc5d9bea397a7b08845228
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-11-11 13:40:33 -0600

    nueva api 13

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-"""
-main.py
-- creates an application instance and runs the dev server
-"""
 # main.py
 if __name__ == '__main__':
     from application import create_app
 
     app = create_app()
 
+    # Agrega un mensaje de depuración para verificar si app se está creando correctamente
+    print("App created successfully")
 
+    app.run(debug=True, port=8056)  # Esto puede no ser necesario si estás usando Gunicorn
